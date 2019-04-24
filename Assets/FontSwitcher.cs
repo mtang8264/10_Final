b2b912a142f3bb83e416d04605e2321532809c7c
Michael Tang <mtang8264@gmail.com>
2019-04-24 12:05:45 -0400

    Made font change slightly randomly

---
+++ Assets/FontSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FontSwitcher : MonoBehaviour
+{
+    public TMP_FontAsset[] fonts;
+    public float avgPerFont;
+    public float variance;
+
+    private float timer;
+    private int selection = 0;
+    private TextMeshPro text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshPro>();
+        timer = avgPerFont + Random.Range(-variance, variance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            timer = avgPerFont + Random.Range(-variance, variance);
+            selection++;
+            if(selection >= fonts.Length)
+            {
+                selection = 0;
+            }
+        }
+
+        text.font = fonts[selection];
+    }
+}
